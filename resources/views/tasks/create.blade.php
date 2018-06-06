2916c2de50b23ffb242fcda28842d9ef400b5e6a
ally <ec2-user@ip-172-31-31-166.us-east-2.compute.internal>
2018-06-06 01:54:29 +0000

    table

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -3,22 +3,27 @@
 
  <h1>タスク新規作成ページ</h1>
  
-   
-    
-   
-
-    {!! Form::model($task, ['route' => 'tasks.store']) !!}
+  
+  {!! Form::model($task, ['route' => 'tasks.store']) !!}
+          <div class="row">
+        <div class="col-xs-12">
+            {!! Form::model($task, ['route' => 'tasks.store']) !!}
+            
+                <div class="form-group">
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
+                    
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+                
+                {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
         
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-        
-        
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('投稿') !!}
-
-    {!! Form::close() !!}
+            {!! Form::close() !!}
+        </div>
+ 
 <!-- Write content for each page here -->
 
 @endsection
