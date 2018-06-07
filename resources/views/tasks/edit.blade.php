63da8f5db6afbc08dbd39cd6636c79b9cc2dc272
ally <ec2-user@ip-172-31-31-166.us-east-2.compute.internal>
2018-06-07 02:59:32 +0000

    Procfile

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -6,7 +6,7 @@
 
     <div class="row">
         <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
-    </div>
+   
             {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
                 <div class="form-group">
                     {!! Form::label('status', 'ステータス:') !!}
@@ -23,7 +23,5 @@
             {!! Form::close() !!}
         </div>
     </div>
-        </div>
-    </div>
-
+        
 @endsection
